9d5f9378ccde5a91015d5a599b290e1d76de8d4e
Manabu ISHII <manabu.ishii.q@gmail.com>
2016-08-26 01:14:21 +0900

    Fix capital code -> Code

--- main_test.go
+++ main_test.go
@@ -13,6 +13,6 @@ func TestHandleIndexReturnsWithStatusOK(t *testing.T) {
 	CityHandler(response, request)
 
 	if response.Code != http.StatusOK {
-		t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.code)
+		t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
 	}
 }
